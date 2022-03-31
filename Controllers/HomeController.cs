7287f8402d22e423269906f826579c5be85b13b3
Jessie Spencer <jeskspencer@gmail.com>
2022-03-30 23:22:32 -0600

    filtering works and working on adding

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,25 +12,49 @@ namespace Mission13.Controllers
 {
     public class HomeController : Controller
     {
-        private IBowlerRepository _repo { get; set; }
-        //Constructor
-        public HomeController(IBowlerRepository temp)
-        {
-            _repo = temp;
-        }
+        //private IBowlerRepository _repo { get; set; }
+        ////Constructor
+        //public HomeController(IBowlerRepository temp)
+        //{
+        //    _repo = temp;
+        //}
 
+        private BowlingDBContext _context { get; set; }
+        
+        public HomeController(BowlingDBContext temp)
+        {
+            _context = temp;
+        }
 
         public IActionResult Index(string bowlerTeam)
         {
             //just a comment so I can make sure everythings committed:)
-            ViewBag.Teams = _repo.Teams.ToList();
+            ViewBag.Teams = _context.Teams.ToList();
 
             var b = new BowlingViewModel
             {
-                Bowlers = _repo.Bowlers
-                    .Where(b => b.Team.TeamName == bowlerTeam)
+                Bowlers = _context.Bowlers
+                    .Where(b => b.Team.TeamName == bowlerTeam || bowlerTeam == null)
             };
             return View(b);
+        }
+
+
+        public IActionResult Entry(Bowler b)
+        {
+            return View();
+            //if (ModelState.IsValid)
+            //{
+            //    _context.Add(b);
+            //    _context.SaveChanges();
+
+            //    return View("Confirmation", b);
+            //}
+            //else
+            //{
+            //    ViewBag.Teams = _context.Teams.ToList();
+            //    return View(b);
+            //}
         }
 
 
--- Models/ViewModels/BowlingViewModel.cs
+++ Models/ViewModels/BowlingViewModel.cs
@@ -8,6 +8,6 @@ namespace Mission13.Models.ViewModels
     public class BowlingViewModel
     {
         public IEnumerable<Bowler> Bowlers { get; set; }
-        public Team Teams { get; set; }
+        public IEnumerable<Team> Teams { get; set; }
     }
 }
